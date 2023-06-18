731a140a98fca58b7f90371487b65433b237716e
Yeicor <4929005+Yeicor@users.noreply.github.com>
2023-06-18 21:09:06 +0200

    Added fallback SVG to PNG renderer for android (due to SAX feature incompatibility on dependency)

--- src/androidMain/kotlin/SvgRender.kt
+++ src/androidMain/kotlin/SvgRender.kt
@@ -1,5 +1,7 @@
 import korlibs.image.bitmap.Bitmap
 import korlibs.image.format.PNG
+import korlibs.logger.Logger
+import org.apache.batik.transcoder.TranscoderException
 import org.apache.batik.transcoder.TranscoderInput
 import org.apache.batik.transcoder.TranscoderOutput
 import org.apache.batik.transcoder.image.PNGTranscoder
@@ -7,9 +9,18 @@ import java.io.ByteArrayOutputStream
 import java.io.StringReader
 
 actual suspend fun svgRender(svgStr: String): Bitmap {
-    val transcoderInput = TranscoderInput(StringReader(svgStr))
-    val baos = ByteArrayOutputStream()
-    val transcoderOutput = TranscoderOutput(baos)
-    PNGTranscoder().transcode(transcoderInput, transcoderOutput)
-    return PNG.decodeSuspend(baos.toByteArray())
+    try {
+        // FIXME: The following code causes a crash due to the feature
+        //  http://apache.org/xml/features/nonvalidating/load-external-dtd
+        //  not existing in android's SAX parser implementation.
+        val transcoderInput = TranscoderInput(StringReader(svgStr))
+        val baos = ByteArrayOutputStream()
+        val transcoderOutput = TranscoderOutput(baos)
+        PNGTranscoder().transcode(transcoderInput, transcoderOutput)
+        return PNG.decodeSuspend(baos.toByteArray())
+    } catch (e: TranscoderException) {
+        Logger("svgRender").error { "Exception: $e. Falling back to default svgRender." }
+        e.printStackTrace()
+    }
+    return svgRenderDefault(svgStr)
 }
